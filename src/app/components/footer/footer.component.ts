694c0a00f7743633da86c41b27b3b107af199844
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-06-26 04:18:02 +0800

    Footer component converted to standalone.

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,7 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  standalone: true,
   selector: 'sca-footer',
+  imports: [
+    CommonModule,
+  ],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
